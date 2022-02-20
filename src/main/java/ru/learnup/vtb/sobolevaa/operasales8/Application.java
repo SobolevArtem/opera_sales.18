465d13c9494175bc77d29d8daec83f2a4edd62a8
SobolevArtem <tema.sobolev@gmail.com>
2022-02-20 17:51:00 +0300

    change of names

--- src/main/java/ru/learnup/vtb/sobolevaa/operasales8/Application.java
+++ src/main/java/ru/learnup/vtb/sobolevaa/operasales8/Application.java
@@ -3,9 +3,7 @@ package ru.learnup.vtb.sobolevaa.operasales8;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import ru.learnup.vtb.sobolevaa.operasales8.services.ConsoleLogger;
 import ru.learnup.vtb.sobolevaa.operasales8.services.EventService;
-import ru.learnup.vtb.sobolevaa.operasales8.services.interfaces.Logger;
 
 @SpringBootApplication
 public class Application {
@@ -15,25 +13,25 @@ public class Application {
 
 		EventService events = ctx.getBean(EventService.class);
 
-		events.addEvent("Test3-1", "проверка №3-1", null,22,18);
-		events.addEvent("Test3-2", "проверка №3-2", null,23,16);
-		events.deleteEvent("Qqqq");
-		events.addEvent("Test1-3", "проверка №3-3", null,5,6);
-		events.getEvent("Test3-1").getSeats().buyAny(12);
+		events.addEvent("Травиата", "Джузеппе Верди", null,20,18);
+		events.addEvent("Кармен", "Жорж Бизе", null,30,16);
+		events.addEvent("Евгений Онегин", "П. И. Чайковский", null,5,6);
+		events.getEvent("Травиата").getSeats().buyAny(12);
 		System.out.println(events);
 
-		events.editEvent("Test3-2",null, null, null,33);
+		events.editEvent("Кармен",null, null, null,33);
 		System.out.println(events);
 
-		events.getEvent("Test1-3").getSeats().sellAny(6);
-		events.editEvent("Test1-3","NEW проверка №3-3", null, 10,14);
-		events.getEvent("Test1-3").getSeats().buyAny(8);
-		events.getEvent("Test1-3").getSeats().sellAny(6);
+		events.getEvent("Евгений Онегин").getSeats().sellAny(6);
+		events.editEvent("Евгений Онегин","П. И. Чайковский (рок-обработка)", null, 10,14);
+		events.getEvent("Евгений Онегин").getSeats().buyAny(8);
+		events.getEvent("Евгений Онегин").getSeats().sellAny(6);
 		System.out.println(events);
 
-		events.deleteEvent("Test3-2");
+		events.deleteEvent("Богема");
+		events.deleteEvent("Кармен");
 		System.out.println(events);
-		System.out.println(events.getEvent("Test1-3").getSeats());
+		System.out.println(events.getEvent("Евгений Онегин").getSeats());
 
 	}
 
